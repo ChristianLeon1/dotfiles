d9e52ff292409bbb031e7b789bebab6a29ad03b4
Christian León <chrisrl604@gmail.com>
2026-02-23 16:07:18 -0600

    pywal conection

--- install.sh
+++ install.sh
@@ -19,10 +19,14 @@ sudo dnf update -y
 echo "Habilitando repositorios RPM Fusion para drivers y multimedia..."
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
                     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+echo "aaaaa"
 # Para Hyprland y herramientas relacionadas
 sudo dnf copr enable solopasha/hyprland 
+
+echo "aaaa1"
 # Para Yazi (terminal file manager)
 sudo dnf copr enable varlad/yazi
+
 
 echo "Instalando dependencias..."
 sudo dnf install -y --allowerasing --skip-broken git stow wget curl gcc gcc-c++ make cmake cargo rust \
